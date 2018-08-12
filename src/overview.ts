03c779832be272102556c72b225578860d0b17c2
hs <>
2018-08-12 11:09:25 -0700

    docs

--- src/overview.ts
+++ src/overview.ts
@@ -4,7 +4,14 @@
  * [`[Github page]`](https://github.com/HelpfulScripts/hsHomeSec)
  * [`[Coverage Info]`](./data/src/hsHomeSec/coverage/)
  * ___
- * 
+ * Written in [Typescript](typescriptlang.org) to run on [Node](nodejs.org).
+ * - supports cameras with built-in web api. Currently tested
+ *     - WansView Q3S, K3
+ *     - Foscam C1
+ * - creates its own ftp server and configures devices to send snapshots and videos there
+ * - communicates with users via email and OSX services Messages, and Facetime 
+ * - simple and universal UI: receives remote user commands via email
+ *
  * For installallation and Setup, see the [README](https://github.com/HelpfulScripts/hsHomeSec) file
  * 
  * 
