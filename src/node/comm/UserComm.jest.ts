ef52f3b2f9f843b37ac9775457899b77ed1dbcb9
hs <>
2018-08-17 11:48:44 -0700

    added actual tests

--- src/node/comm/UserComm.jest.ts
+++ src/node/comm/UserComm.jest.ts
@@ -1,1 +1,13 @@
-test('dummy test', () => expect("hey!").toBe("hey!"));
+import * as user from './UserComm';
+
+
+describe('UserComm', () => {
+    const u = {name:'Dude', email:['me@dude.com']};
+    user.users.addUser(u);
+    it('should have user', () => {
+        expect(user.users.userByName('Dude')).toEqual(u);
+    });
+    it('should have user', () => {
+        expect(user.users.userByEmail('me@dude.com')).toEqual(u);
+    });
+});
