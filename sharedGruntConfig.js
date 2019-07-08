66b11d91604ac5665f21dee8a4d6ea75ac18a026
hs <helpfulscripts@gmail.com>
2019-07-07 21:26:32 -0700

    excluded node apps from webPack

--- sharedGruntConfig.js
+++ sharedGruntConfig.js
@@ -64,7 +64,7 @@ module.exports = (grunt, dir, dependencies, type, lib) => {
     grunt.registerTask('default',       ['product']);	
     grunt.registerTask('dev',           ['buildDev', 'stage']);
     grunt.registerTask('product',       ['buildMin', 'stage']);	
-    grunt.registerTask('travis',        ['build-base', 'build-js', 'webpack:appDev', 'webpack:appProd', 'test']);	
+    grunt.registerTask('travis',        ['build-base', 'build-js', (type === 'node')?'':'webpack:appProd', 'test']); // exlude node-apps from webPack to avoid webpack error
     grunt.registerTask('help',          ['h']);	
 
     grunt.registerMultiTask('sourceCode', translateSourcesToHTML);  
